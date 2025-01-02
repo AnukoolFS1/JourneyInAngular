94dfaf220f56668e3cf1af69e26ef36613042c3c
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-02 22:30:19 +0530

    first change to angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+// import { RouterOutlet } from '@angular/router';
+import { WishItems } from '../shared/models/Wishitems';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  items = [new WishItems("Learn Angular"), new WishItems("Learn Typescript")]
   title = 'wishlist';
 }
---
+++ src/shared/models/Wishitems.ts
@@ -0,0 +1,5 @@
+export class WishItems {
+    constructor(public wishText: string, public isComplete: boolean = false){
+        
+    }
+}
