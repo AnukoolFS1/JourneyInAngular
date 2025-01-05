1df5e02786e2f1b3835ea0661b209bf5d224b5bb
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-01-06 00:11:30 +0530

    setTitle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 // import { RouterOutlet } from '@angular/router';
 import { WishItems } from '../shared/models/Wishitems';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -12,4 +13,8 @@ import { WishItems } from '../shared/models/Wishitems';
 export class AppComponent {
   items = [new WishItems("Learn Angular"), new WishItems("Learn Typescript")]
   title = 'wishlist';
+
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Learning the Angular'); // Set the initial page title
+  }
 }
