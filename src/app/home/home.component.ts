688804a0a90c09bb9844a14c8a1d2307e4c4b6ff
Anukool Singh Chauhan <ascmailing0@gmail.com>
2025-02-18 15:48:19 +0530

    Interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [CommonModule, HousingLocationComponent],
   templateUrl: `./home.component.html`,
   styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = 'https://angular.dev/assets/images/tutorials/common';
+
+  housingLocation: HousingLocation = {
+    id: 999,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'St',
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false
+  };
+}
